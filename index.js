5366ebe5dd63565e1cc26b008357ec4ecaef44e8
Sriya77 <sriyachakinala.com>
2024-12-16 13:15:33 +0530

    v3

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require('express')
+// 
+
+
+const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 let leet = require('./leetcode');
 const path = require('path');
-app.use(express.static('public'));
 
+// Enable CORS
 app.use(cors({
     origin: '*'
-}))
-console.log("hello");
+}));
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+// Optional: Serve index.html as a fallback for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Dynamic route for "/:id"
 app.get('/:id', leet.leetcode);
 
+// Start the server
 app.listen(3001, () => {
     console.log(`App is running on port 3001`);
 });
