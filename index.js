529ca166b44558a34fa90043071070c18051557f
Sriya77 <sriyachakinala.com>
2024-12-16 13:38:30 +0530

    finalverion01

--- index.js
+++ index.js
@@ -1,26 +1,112 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-let leet = require('./leetcode');
-const path = require('path');
 
-// Enable CORS
+const app = express();
 app.use(cors({
-    origin: '*'
+    origin: '*' // Adjust the CORS policy as needed
 }));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+const query = `
+  query getUserProfile($username: String!) {
+    allQuestionsCount {
+      difficulty
+      count
+    }
+    matchedUser(username: $username) {
+      contributions {
+        points
+      }
+      profile {
+        reputation
+        ranking
+      }
+      submissionCalendar
+      submitStats {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
+        }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+        }
+      }
+    }
+    recentSubmissionList(username: $username) {
+      title
+      titleSlug
+      timestamp
+      statusDisplay
+      lang
+      __typename
+    }
+    matchedUserStats: matchedUser(username: $username) {
+      submitStats: submitStatsGlobal {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        __typename
+      }
+    }
+  }
+`;
 
-// Optional: Serve index.html as a fallback for the root route
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-// 
-// Dynamic route for "/:id"
-app.get('/:id', leet.leetcode);
+const formatData = (data) => {
+  return {
+    totalSolved: data.matchedUserStats.submitStats.acSubmissionNum[0].count,
+    totalSubmissions: data.matchedUser.submitStats.totalSubmissionNum,
+    totalQuestions: data.allQuestionsCount[0].count,
+    easySolved: data.matchedUser.submitStats.acSubmissionNum[1].count,
+    totalEasy: data.allQuestionsCount[1].count,
+    mediumSolved: data.matchedUser.submitStats.acSubmissionNum[2].count,
+    totalMedium: data.allQuestionsCount[2].count,
+    hardSolved: data.matchedUser.submitStats.acSubmissionNum[3].count,
+    totalHard: data.allQuestionsCount[3].count,
+    ranking: data.matchedUser.profile.ranking,
+    contributionPoint: data.matchedUser.contributions.points,
+    reputation: data.matchedUser.profile.reputation,
+    submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar),
+    recentSubmissions: data.recentSubmissionList,
+    matchedUserStats: data.matchedUser.submitStats,
+  };
+};
 
-// Start the server
-app.listen(3001, () => {
-    console.log(`App is running on port 3001`);
+app.get('/leetcode/:id', (req, res) => {
+  const user = req.params.id;
+  fetch('https://leetcode.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Referer': 'https://leetcode.com',
+    },
+    body: JSON.stringify({ query, variables: { username: user } }),
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      if (data.errors) {
+        res.send({ data });
+      } else {
+        res.send(formatData(data.data));
+      }
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      res.status(500).send(err);
+    });
+});
+
+// Start the backend server
+app.listen(4000, () => {
+  console.log('LeetCode Backend is running on port 4000');
 });
